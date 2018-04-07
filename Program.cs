88704b23c5d49b6dc4e4986379e494ba7780cf4f
maximus <maxcamer@gmail.com>
2018-04-07 18:57:06 +0300

    1000

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VySeconddApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			short x  = 1000;
+			string str = Convert.ToString(x);			
+			Console.WriteLine(x);
+			Console.ReadKey();
+        }
+    }
+}
+
+
