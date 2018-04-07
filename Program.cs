57f909dd51c69140188026f7ed97f45a96088316
maximus <maxcamer@gmail.com>
2018-04-07 23:13:25 +0300

    #5

--- Program.cs
+++ Program.cs
@@ -8,15 +8,19 @@ namespace VySeconddApp2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+		static void Main(string[] args)
+		{
 			Console.WriteLine("040273");
 			string text = Console.ReadLine();
 			int x = Convert.ToInt32(text);
-			if(x > 2017)
+			if (x > 2017)
 			{
 				Console.WriteLine("Ты либо из будущего, либо балобол.");
 				Console.WriteLine("Что то я не вижу рядом машину времени...");
+			}
+			else if (x > 2010)
+			{
+				Console.WriteLine("Ути пути какой малыш");
 			}
 		
 			Console.ReadKey();
